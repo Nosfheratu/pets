0e8ff960868219e8c2253babbe78141934959cc4
Fernando Serapio <fhernandosc@gmail.com>
2016-08-07 19:34:08 -0500

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :pets
+  get 'home/link' => 'home#link'
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
