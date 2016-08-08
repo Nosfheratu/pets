9a067d70e02b36932274d490de29d1ef320ee390
Fernando Serapio <fhernandosc@gmail.com>
2016-08-07 19:34:30 -0500

    adds home and pets controllers

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  respond_to :html, :js
+  def index
+  end
+
+  def link
+  end
+end
---
+++ app/views/home/link.coffee
@@ -0,0 +1,2 @@
+$('#pets_report').attr('href', '<%= pets_path %>')
+console.log "setting <%= pets_path %> path to button"
